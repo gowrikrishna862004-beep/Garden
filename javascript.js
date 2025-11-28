7e73129c61a570ca1894445aff3c34e4b9557812
git config --global user.name gowrikrishna862004-beep <gowrikrishna862004@gmail.com>
2025-11-28 12:43:39 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,374 @@
+// Object
+// let student={
+//     name:"ram",
+//     age:22,
+//     isPresent:true,
+// }
+// console.log(student);
+
+// Array
+// let days=['monday','tuesday','wednesday'];
+// console.log(day
+
+// function welcome(){
+    // document.write("welcome to javascript class");
+
+// }
+// welcome();
+// welcome();
+
+// function display(name){
+//     document.write("welcome"+" "+name);
+        
+    
+// }
+// display("john");
+// display("have a nice day")
+
+// assignment operator
+// add
+// var x= 10;
+// x+=4 ;
+// document.write(x);
+
+// sub
+// var x=6;
+// x-=4;
+// document.write(x);
+
+// multiply
+// var y=12;
+// y*=4;
+// document.write(y);
+
+// division
+// d=10;
+// d/=2;
+// document.write(d);
+
+// modula
+// y=30;
+// y%=8;
+// document.write(y);
+
+
+// string comparison
+
+// let text1 ="a";
+// let text2 ="B";
+// let result =text1<text2;
+// document.write("is a less than B?"+result);
+
+// string addition
+// let text1="lotus";
+// let text2="rose";
+// let text3= text1+""+text2;
+// document.write(text3);
+
+// built in function
+// let name="ragu";
+// document.write(typeof(name));
+
+// string convertion
+
+// let v1=123;
+// let v2=333;
+// document.write(typeof v1);
+// document.write(typeof String(v1));
+// document.write(typeof v2.toString());
+
+// values to number
+// let v="144";
+// document.write(typeof v);
+// document.write(typeof Number(v));/
+
+// prompt() method
+// let name=prompt("what is your name?");
+// alert("hello,"+name+"!");
+
+// confirm()method
+
+// let answer=confirm("do you want to continue?");
+// if (answer){
+    // alert("you chose to continue!");
+// 
+// } else{
+    // alert("you cancelled.");
+// }
+// for loop
+
+// for(let i=0;i<4;i++){
+    // document.write(i);
+// }
+
+// while loop
+// let i=0;
+// while(i<6){
+    // document.write(i);
+    // i++
+// }
+// do while loop
+// let i=0;
+// do{
+    // document.write("welcome");
+    // i++;
+// }
+// while(i>6);
+
+// if else
+
+    // number=2
+    // if(number>=0)
+    // {
+    //     document.write("number is positive")
+    // }
+    // else
+    // {
+    //     document.write("number is negative")
+    // }
+
+    // switch/
+    //  let day=5;
+    //  switch(day){
+    //     case 1:
+    //         document.write("monday");
+    //         break;
+    //         case 2:
+    //         document.write("tuesday");
+    //         break;
+    //         case 3:
+    //         document.write("wednesday");
+    //         break;
+    //         case 4:
+    //         document.write("thursday");
+    //         break;
+    //         default:
+    //             document.write("invalid day");
+
+
+
+
+    //  }
+    // nested
+    // let mark=60
+    // if(mark>=40){
+    //     if(mark>=75){
+    //         document.write("first class");}
+    //         else{
+    //             document.write("pass");
+    //         }
+    //     }
+    
+    // else{
+    //     document.write("fail");
+    // }
+    // elements by 
+    
+    // var a=document.getElementById('head');
+    // console.log(a);
+
+    // var p=document.getElementById('para');
+    // para.innerhtml="<h1>welcome</h1>";
+
+    // var a=document.getElementById('p');
+    // a.innerText="welcome";
+    // // console.log(a);
+
+    // var a=document.getElementById('para');
+    // a.style.color="red";
+
+    // var heading=document.getElementsByTagName('h1');
+    // heading[0].innerText="welcome";
+
+    // var heading=document.getElementsByClassName('head');
+    // heading[0].innertext="welcome";
+
+    // var x=document.querySelector("#para");
+    // x.style.color="red";
+
+    // var x=document.querySelectorAll(".colors")
+    
+
+    // function myFunction(x,y){
+    //     return x+y
+    // }
+    // const value = myFunction(8,3)
+    
+    // document.write(value)
+// 
+    // function clicked()
+        // document.getElementById("head").style.backgroundColor="red"</button>
+
+       
+    
+            // var x=document.getElementById("press");
+            // function fun(){
+            //     var a=1;
+            //     try{
+            //         adlert("value of a:"+a);
+            //     }
+            //     catch(e)
+            //     {
+            //         console.log("error:"+e);
+                        
+                    
+            //     }
+
+            // var x= document.getElementById("press"); 
+            // function fun(){
+            //     var a=1;
+            //     try{
+            //         if(a!=2) throw "values are not equal"
+            //     }
+            //     catch(e)
+            //     {
+            //         console.log("error is:"+e);
+            //     }
+            //     finally{
+            //         console.log("the checking completed succesfully")
+            //     }
+            // }
+
+            // let grade ="A"
+            // switch(grade){
+                
+            // case "A":
+            // document.write("excellent");
+            // break;
+                    
+            // case "B":
+            // document.write("very good");
+            // break;
+
+            // case "C":
+            // document.write("good");
+            // break;
+            
+            // default:
+            // document.write("invalid grade");
+            // }
+            // let name ="gowri"
+            // console.log("gowri");
+
+            // var a=1;
+            // var b=2;
+            // console.log(a+b);
+
+            // let student={name:"gowri",age:21};
+            // document.write( student.name);
+            // document.write(student.age);
+
+            // function welcome(name) {
+                // document.write( "hello"+" "+ name)
+            // }
+            // welcome("gowri");
+            // welcome("dariha");
+            // welcome("babin");
+
+            // for(var a=0;a<=10;a++){
+            // document.write(a);
+            // }
+
+            // let a=3;
+            // if(a<0)
+            // document.write("number is positive")
+
+            // else if(a>0){
+            //     document.write("number is negative ")
+            // }
+
+
+            // else
+            // {
+                // document.write("number is zero")
+            // }
+
+            // name
+            // let name ="apple"
+            // console.log("apple");
+
+            // add
+            // var a=10;
+            // var b=20;
+            // console.log(a+b);
+
+            // function
+
+            // function name(rainbow){
+                // document.write("colours" + " " +rainbow)
+            // }
+            // name("red");
+            // name("blue");
+            // name("yellow");
+
+            // forloop
+            // for(let i=0;i>4;i++);{
+            // document.write(i);
+            // }
+            // whileloop
+
+            // let i=0;
+            // while(i<6){
+                // document.write(i);
+                // i++
+            // }
+
+            // do whileloop
+
+            let i=0;
+            do{
+                document.write("welcome");
+                i++;
+            }
+            while(i>6);
+
+        
+            
+            
+
+
+
+            
+            
+
+
+    
+
+        
+
+
+
+
+
+
+            
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
